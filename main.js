73e1647fcc5c49a89fd57ee86adbcf7216535d16
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-13 22:01:37 +0900

    イベントボタンを押した後のイベント処理実装

--- main.js
+++ main.js
@@ -3,7 +3,26 @@
     const btn = document.getElementById('btn');
     btn.addEventListener('click', ()=> {
         const result = document.getElementById('result');
-        result.textContent = 'clicked';
+        const result_text = document.getElementById('result_text');
+        const omikuji = [
+            '大吉',
+            '中吉',
+            '小吉',
+            '吉',
+            '凶',
+            '大凶',
+        ];
+        const omikujiText = [
+            '大吉の場合の処理が入ります。結果がこちらに入ります。',
+            '中吉の場合の処理が入ります。結果がこちらに入ります。',
+            '小吉の場合の処理が入ります。結果がこちらに入ります。',
+            '吉の場合の処理が入ります。結果がこちらに入ります。',
+            '凶の場合の処理が入ります。結果がこちらに入ります。',
+            '大凶の場合の処理が入ります。結果がこちらに入ります。',
+        ];
+        const r_math = Math.floor(Math.random()*omikuji.length);
+        result.textContent = omikuji[r_math];
+        result_text.textContent = omikujiText[r_math];
     },false);
 
 
