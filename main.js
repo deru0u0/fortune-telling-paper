f2815cb42a0f65d84ffd55077739bd5935baef86
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-01-13 21:51:51 +0900

    ボタンを押したときのイベント処理実装

---
+++ main.js
@@ -0,0 +1,13 @@
+'use script';
+{
+    const btn = document.getElementById('btn');
+    btn.addEventListener('click', ()=> {
+        const result = document.getElementById('result');
+        result.textContent = 'clicked';
+    },false);
+
+
+
+
+
+}
