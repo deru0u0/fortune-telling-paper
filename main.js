5b99fe234c01f2a288da3b47487a4805c25ac1f2
Mao Tsunoda <msl.0626.u3u.hoge@gmail.com>
2024-03-01 15:59:44 +0900

    修正

--- main.js
+++ main.js
@@ -1,38 +1,13 @@
-'use script';
+'use strict';
 {
-    const omikujiObj = {
-        title:"おみくじ",
-        omikujiItem: [
-            '大吉',
-            '中吉',
-            '小吉',
-            '吉',
-            '凶',
-            '大凶',
-        ],
-        omikuji: function() {
-            const resultItem = document.getElementById('result');
-            const r_math = Math.floor(Math.random()*this.omikujiItem.length);
-            resultItem.textContent = this.omikujiItem[r_math];
-        },
-
-        detailMessageButton: function() {
-            const omikujiDetailMessageButton = document.getElementById('omikujiButton');
-            omikujiDetailMessageButton.value = 'もう一度占う';
-            omikujiDetailMessageButton.id = 'detailMessageButton';
-        }
-    }
-    const h1 = document.querySelector('h1');
-    h1.textContent = omikujiObj.title;
-    const omikujiButton = document.getElementById('omikujiButton');
-
-    omikujiButton.addEventListener('click', (e)=> {
-        if ( e.target.id === "omikujiButton" ) {
-            omikujiObj.omikuji();
-            omikujiObj.detailMessageButton();
+    let isSwitch = true;
+    document.querySelector('#discription_btn').addEventListener('click', function() {
+        const switch_display = document.querySelector('.omikuji-image-slider-container');
+        if ( isSwitch ) {
+            switch_display.classList.add('show-display');
         } else {
-            location.reload();
+            switch_display.classList.remove('show-display');
         }
+        isSwitch = !isSwitch;
     },false);
-    
 }
